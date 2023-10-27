ae0e3f5937959cd5afea95a217ca25963be347f3
tastaw23 <younesmekki2@gmail.com>
2023-10-26 21:51:14 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -13,3 +13,15 @@ void print_binary(unsigned long int n)
 	{
 		current = n >> i;
 
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
+

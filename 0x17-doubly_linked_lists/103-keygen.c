629ccfc57f91fc305eeb1592eb0f8b67ab6e0172
tastaw23 <younesmekki2@gmail.com>
2023-12-07 05:00:14 -0800

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -5,56 +5,56 @@
 
 /**
  * generate_password - Generates a password based on the input string.
- * @input_str: The input string.
- * @output_password: The buffer to store the generated password.
+ * @input: The input string.
+ * @password: The buffer to store the generated password.
  */
-void generate_password(const char *input_str, char *output_password) {
-    const char *character_set = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
-    int input_length = strlen(input_str);
-    int i, temp;
+void generate_password(const char *input, char *password) {
+    const char *codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+    int len = strlen(input);
+    int i, tmp;
 
     // Operation 1
-    temp = (input_length ^ 59) & 63;
-    output_password[0] = character_set[temp];
+    tmp = (len ^ 59) & 63;
+    password[0] = codex[tmp];
 
     // Operation 2
-    temp = 0;
-    for (i = 0; i < input_length; i++)
-        temp += input_str[i];
-    output_password[1] = character_set[(temp ^ 79) & 63];
+    tmp = 0;
+    for (i = 0; i < len; i++)
+        tmp += input[i];
+    password[1] = codex[(tmp ^ 79) & 63];
 
     // Operation 3
-    temp = 1;
-    for (i = 0; i < input_length; i++)
-        temp *= input_str[i];
-    output_password[2] = character_set[(temp ^ 85) & 63];
+    tmp = 1;
+    for (i = 0; i < len; i++)
+        tmp *= input[i];
+    password[2] = codex[(tmp ^ 85) & 63];
 
     // Operation 4
-    temp = 0;
-    for (i = 0; i < input_length; i++) {
-        if (input_str[i] > temp)
-            temp = input_str[i];
+    tmp = 0;
+    for (i = 0; i < len; i++) {
+        if (input[i] > tmp)
+            tmp = input[i];
     }
-    srand(temp ^ 14);
-    output_password[3] = character_set[rand() & 63];
+    srand(tmp ^ 14);
+    password[3] = codex[rand() & 63];
 
     // Operation 5
-    temp = 0;
-    for (i = 0; i < input_length; i++)
-        temp += (input_str[i] * input_str[i]);
-    output_password[4] = character_set[(temp ^ 239) & 63];
+    tmp = 0;
+    for (i = 0; i < len; i++)
+        tmp += (input[i] * input[i]);
+    password[4] = codex[(tmp ^ 239) & 63];
 
     // Operation 6
-    for (i = 0; i < input_str[0]; i++)
-        temp = rand();
-    output_password[5] = character_set[(temp ^ 229) & 63];
+    for (i = 0; i < input[0]; i++)
+        tmp = rand();
+    password[5] = codex[(tmp ^ 229) & 63];
 
     // Null-terminate the password
-    output_password[6] = '\0';
+    password[6] = '\0';
 }
 
 int main(int argc, char *argv[]) {
-    char generated_password[7];
+    char password[7];
 
     // Check if username is provided as a command-line argument
     if (argc != 2) {
@@ -63,10 +63,10 @@ int main(int argc, char *argv[]) {
     }
 
     // Generate password based on the input username
-    generate_password(argv[1], generated_password);
+    generate_password(argv[1], password);
 
     // Print the generated password
-    printf("%s\n", generated_password);
+    printf("%s\n", password);
 
     return EXIT_SUCCESS;
 }

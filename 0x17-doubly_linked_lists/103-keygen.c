33a3fe38876ccacfb8bf142ce3c81b8eefd53157
tastaw23 <younesmekki2@gmail.com>
2023-12-07 05:01:14 -0800

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,73 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
 
 /**
- * generate_password - Generates a password based on the input string.
- * @input: The input string.
- * @password: The buffer to store the generated password.
+ * main - Generates and prints passwords for the crackme5 executable.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
  */
-void generate_password(const char *input, char *password) {
-    const char *codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
-    int len = strlen(input);
-    int i, tmp;
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	char password[7], *codex;
+	int len = strlen(argv[1]), i, tmp;
 
-    // Operation 1
-    tmp = (len ^ 59) & 63;
-    password[0] = codex[tmp];
+	codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 
-    // Operation 2
-    tmp = 0;
-    for (i = 0; i < len; i++)
-        tmp += input[i];
-    password[1] = codex[(tmp ^ 79) & 63];
+	tmp = (len ^ 59) & 63;
+	password[0] = codex[tmp];
 
-    // Operation 3
-    tmp = 1;
-    for (i = 0; i < len; i++)
-        tmp *= input[i];
-    password[2] = codex[(tmp ^ 85) & 63];
+	tmp = 0;
+	for (i = 0; i < len; i++)
+		tmp += argv[1][i];
+	password[1] = codex[(tmp ^ 79) & 63];
 
-    // Operation 4
-    tmp = 0;
-    for (i = 0; i < len; i++) {
-        if (input[i] > tmp)
-            tmp = input[i];
-    }
-    srand(tmp ^ 14);
-    password[3] = codex[rand() & 63];
+	tmp = 1;
+	for (i = 0; i < len; i++)
+		tmp *= argv[1][i];
+	password[2] = codex[(tmp ^ 85) & 63];
 
-    // Operation 5
-    tmp = 0;
-    for (i = 0; i < len; i++)
-        tmp += (input[i] * input[i]);
-    password[4] = codex[(tmp ^ 239) & 63];
+	tmp = 0;
+	for (i = 0; i < len; i++)
+	{
+		if (argv[1][i] > tmp)
+			tmp = argv[1][i];
+	}
+	srand(tmp ^ 14);
+	password[3] = codex[rand() & 63];
 
-    // Operation 6
-    for (i = 0; i < input[0]; i++)
-        tmp = rand();
-    password[5] = codex[(tmp ^ 229) & 63];
+	tmp = 0;
+	for (i = 0; i < len; i++)
+		tmp += (argv[1][i] * argv[1][i]);
+	password[4] = codex[(tmp ^ 239) & 63];
 
-    // Null-terminate the password
-    password[6] = '\0';
+	for (i = 0; i < argv[1][0]; i++)
+		tmp = rand();
+	password[5] = codex[(tmp ^ 229) & 63];
+
+	password[6] = '\0';
+	printf("%s", password);
+	return (0);
 }
-
-int main(int argc, char *argv[]) {
-    char password[7];
-
-    // Check if username is provided as a command-line argument
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s username\n", argv[0]);
-        return EXIT_FAILURE;
-    }
-
-    // Generate password based on the input username
-    generate_password(argv[1], password);
-
-    // Print the generated password
-    printf("%s\n", password);
-
-    return EXIT_SUCCESS;
-}
-

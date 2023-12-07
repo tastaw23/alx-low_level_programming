dc8cf33999082d7f10f12631877a97f343fa2156
tastaw23 <younesmekki2@gmail.com>
2023-12-07 02:02:13 -0800

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -18,8 +18,10 @@ typedef struct dlistint_s
     struct dlistint_s *next;
 } dlistint_t;
 
-/* Function prototypes */
-size_t dlistint_len(const dlistint_t *h);
+/* Function prototype for print_dlistint */
+size_t print_dlistint(const dlistint_t *h);
+
+/* Add other function prototypes as needed */
 
 #endif /* LISTS_H */
 

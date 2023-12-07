952fc6833ffc884c992d72438b278ba60f960262
tastaw23 <younesmekki2@gmail.com>
2023-12-07 06:01:23 -0800

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,50 +11,50 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *new_node, *temp = *h;
-    unsigned int i;
+	dlistint_t *new_node, *temp = *h;
+	unsigned int i;
 
-    new_node = malloc(sizeof(dlistint_t));
-    if (!h || !new_node)
-    {
-        return (NULL);
-    }
+	new_node = malloc(sizeof(dlistint_t));
+	if (!h || !new_node)
+	{
+		return (NULL);
+	}
 
-    new_node->n = n;
+	new_node->n = n;
 
-    if (idx == 0)
-    {
-        new_node->prev = NULL;
-        new_node->next = *h;
-        if (*h)
-        {
-            (*h)->prev = new_node;
-        }
-        *h = new_node;
-        return (new_node);
-    }
+	if (idx == 0)
+	{
+		new_node->prev = NULL;
+		new_node->next = *h;
+		if (*h)
+		{
+			(*h)->prev = new_node;
+		}
+		*h = new_node;
+		return (new_node);
+	}
 
-    for (i = 0; temp && i < idx - 1; ++i)
-    {
-        temp = temp->next;
-    }
+	for (i = 0; temp && i < idx - 1; ++i)
+	{
+		temp = temp->next;
+	}
 
-    if (!temp)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	if (!temp)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    new_node->prev = temp;
-    new_node->next = temp->next;
+	new_node->prev = temp;
+	new_node->next = temp->next;
 
-    if (temp->next)
-    {
-        temp->next->prev = new_node;
-    }
+	if (temp->next)
+	{
+		temp->next->prev = new_node;
+	}
 
-    temp->next = new_node;
+	temp->next = new_node;
 
-    return (new_node);
+	return (new_node);
 }
 

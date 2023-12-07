3047e909e386836ba540e71a1ad72e2aa3413503
tastaw23 <younesmekki2@gmail.com>
2023-12-07 03:07:11 -0800

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,30 +8,30 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node;
+	dlistint_t *new_node;
 
-    if (head == NULL)
-        return NULL;
+	if (head == NULL)
+		return (NULL);
 
-    /* Allocate memory for the new node */
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-        return NULL;
+	/* Allocate memory for the new node */
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    /* Set the value of the new node */
-    new_node->n = n;
+	/* Set the value of the new node */
+	new_node->n = n;
 
-    /* Update pointers to insert the new node at the beginning */
-    new_node->next = *head;
-    new_node->prev = NULL;
+	/* Update pointers to insert the new node at the beginning */
+	new_node->next = *head;
+	new_node->prev = NULL;
 
-    /* Update the previous of the current head if it's not NULL */
-    if (*head != NULL)
-        (*head)->prev = new_node;
+	/* Update the previous of the current head if it's not NULL */
+	if (*head != NULL)
+		(*head)->prev = new_node;
 
-    /* Update the head to point to the new node */
-    *head = new_node;
+	/* Update the head to point to the new node */
+	*head = new_node;
 
-    return new_node;
+	return (new_node);
 }
 

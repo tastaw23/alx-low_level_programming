3f8d08793f5a6138b8155b318d3560aca08d048c
tastaw23 <younesmekki2@gmail.com>
2024-02-21 09:40:30 -0800

    air

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,30 +1,23 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include 'main.h'
 
-/**
- * main - Determines if a number is positive, negative or zero.
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int n;
+void positive_or_negative(int i) {
+    if (i > 0) {
+        std::cout << i << " is positive" << std::endl;
+    } else if (i == 0) {
+        std::cout << i << " is zero" << std::endl;
+    } else {
+        std::cout << i << " is negative" << std::endl;
+    }
+}
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
-	{
-		printf("%d is negative\n", n);
-	}
-	return (0);
+int main() {
+    int number;
+
+    std::cout << "Enter an integer: ";
+    std::cin >> number;
+
+    positive_or_negative(number);
+
+    return 0;
 }
 
